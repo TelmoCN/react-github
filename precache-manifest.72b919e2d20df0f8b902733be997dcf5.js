c4763fae71c988ee8c425f999d35b8013a005953
Telmo Sampaio <telmo.sampaio@wearecodenation.com>
2019-11-21 11:06:11 +0000

    Updates

---
+++ precache-manifest.72b919e2d20df0f8b902733be997dcf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25099b1e0ea1fa235d89ac2fb2954646",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "e72c9915ef12e65ece6a",
+    "url": "/react-github/static/css/main.5b3b90ab.chunk.css"
+  },
+  {
+    "revision": "17f87e623136edf9c99c",
+    "url": "/react-github/static/js/2.a2a9193f.chunk.js"
+  },
+  {
+    "revision": "e72c9915ef12e65ece6a",
+    "url": "/react-github/static/js/main.435bb3ea.chunk.js"
+  },
+  {
+    "revision": "1d33c38306cc33ea9db5",
+    "url": "/react-github/static/js/runtime-main.139dd5ec.js"
+  }
+]);
